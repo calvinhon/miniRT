0d0ce1aba1e99d0de2130a592eb4524a10fb4a6a
hoach nguyen <nthoacht@gmail.com>
2025-01-22 13:02:01 +0400

    add new parse, maths, hooks,libft, main,

--- include/lighting_and_shading.h
+++
@@ -1,6 +0,0 @@
-#include "minirt_math.h"
-#include "colors.h"
-
-t_vec4d	normal_at(t_sphere s, t_point wrld_p);
-t_vec4d	reflect(t_vec4d in, t_vec4d normal);
-t_color	lighting(t_sphere s, t_light l, t_point p, t_vec4d eye_v);
