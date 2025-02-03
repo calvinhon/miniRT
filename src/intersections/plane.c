20f0c774cd19ddbad9da5bf9a45c8bb19967873e
Calvinhon <calvin.r.hon@gmail.com>
2025-02-03 17:43:13 +0400

    bump maps work

--- src/intersections/plane.c
+++ src/intersections/plane.c
@@ -12,18 +12,11 @@
 
 #include "minirt.h"
 
-t_vec4d	plane_normal_at(t_object *o, t_point *wrld_p)
+t_vec4d plane_normal_at(t_object *o, t_point *wrld_p)
 {
-	t_vec4d	obj_normal;
-	t_vec4d	wrld_normal;
-	t_mat4d	transposed;
-
-	(void)o;
-	(void)wrld_p;
-	obj_normal = create_vec4d(0, 1, 0);
-	transposed = transpose_mat4d(&o->inv_transform);
-	wrld_normal = mult_mat4d_vec4d(&transposed, &obj_normal);
-	return (normalize(&wrld_normal));
+	if (o->material.bump_map)
+		return (apply_bump_map(wrld_p, o->material.bump_map, &o->orientation));
+	return (o->orientation);
 }
 
 void	intersect_plane(t_ray *r, t_object *o, t_itx_grp *xs)
