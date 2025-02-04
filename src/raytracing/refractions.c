fd1878c711c4691eb5c1e5a26469fa2fb9b41691
Calvinhon <calvin.r.hon@gmail.com>
2025-02-04 20:48:06 +0400

    refraction test pass

--- src/raytracing/refractions.c
+++ src/raytracing/refractions.c
@@ -20,34 +20,53 @@ void	swap_itx(t_itx *itx_a, t_itx *itx_b)
 	*itx_b = temp_itx;
 }
 
-bool	is_container(t_list *containers, t_object *o)
+bool	is_container(t_list **containers, t_object *o)
 {
-	t_list	*head;
+	t_list	*node;
 	t_list	*tmp;
 
-	head = containers;
-	while (containers)
+	node = *containers;
+	tmp = node;
+	if (&((t_object *)node->content)->type == &o->type)
 	{
-		if (&((t_object *)containers->content)->type == &o->type)
+		ft_lstdelone(node, NULL);
+		*containers = NULL;
+		return (1);
+	}
+	while (node)
+	{
+		if (node->next && &((t_object *)node->next->content)->type == &o->type)
 		{
-			while (head->next && &((t_object *)(head->next->content))->type != &o->type)
-				head = head->next;
-			if (head->next && head->next->next)
+			if (node->next->next)
 			{
-				tmp = head->next->next;
-				ft_lstdelone(head->next, NULL);
-				head->next = tmp;
+				tmp = node->next->next;
+				ft_lstdelone(node->next, NULL);
+				node->next = tmp;
 			}
-			else
+			else if (node->next)
 			{
-				ft_lstdelone(head->next, NULL);
-				head->next = NULL;
+				ft_lstdelone(node->next, NULL);
+				node->next = NULL;
 			}
 			return (1);
 		}
-		containers = containers->next;
+		node = node->next;
 	}
 	return (0);
+}
+
+void	sort_xs(t_itx_grp *xs)
+{
+	int	i;
+
+	i = -1;
+	while (!is_ordered(xs))
+	{
+		while (++i < xs->count - 1)
+			if (xs->arr[i].t > xs->arr[i + 1].t)
+				swap_itx(&xs->arr[i], &xs->arr[i + 1]);
+		i = -1;
+	}
 }
 
 void	prepare_refractions(t_itx *itx, t_comps *comps, t_itx_grp *xs)
@@ -57,29 +76,25 @@ void	prepare_refractions(t_itx *itx, t_comps *comps, t_itx_grp *xs)
 
 	(void)itx;
 	(void)comps;
-	i = -1;
-	while (!is_ordered(xs))
-	{
-		while (++i < xs->count - 1)
-			if (xs->arr[i].t > xs->arr[i + 1].t)
-				swap_itx(&xs->arr[i], &xs->arr[i + 1]);
-		i = -1;
-	}
+	sort_xs(xs);
 	containers = NULL;
 	i = -1;
 	while (++i < xs->count)
 	{
 		if (xs->arr[i].obj->material.refractive)
 		{
-			printf("i: %d\n", i);
 			if (!containers)
 				xs->arr[i].n1 = 1;
 			else
-				xs->arr[i].n1 = ((t_object *)(ft_lstlast(containers)->content))->material.refractive;
-			if (!containers || !is_container(containers, xs->arr[i].obj))
+				xs->arr[i].n1 =
+					((t_object *)(ft_lstlast(containers)->content))->material.refractive;
+			if (!containers || !is_container(&containers, xs->arr[i].obj))
 				ft_lstadd_back(&containers, ft_lstnew(xs->arr[i].obj));
+			if (!containers)
+				xs->arr[i].n2 = 1;
+			else
+				xs->arr[i].n2 =
+					((t_object *)(ft_lstlast(containers)->content))->material.refractive;
 		}
 	}
-	printf("list length: %d\n", ft_lstsize(containers));
-	ft_lstclear(&containers, NULL);
 }
